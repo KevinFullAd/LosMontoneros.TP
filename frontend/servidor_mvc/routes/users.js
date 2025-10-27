c4cc6490da415dfc1b5dd34488aab76bd8c147c4
Facundo-Dev <facundofdf@hotmail.com>
2025-10-27 09:48:21 -0300

    chore: Se eliminan carpetas duplicadas del back y front.

--- frontend/servidor_mvc/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-//GET /users
-router.get('/', (req, res) => {
-    res.render('Ruta de bienvenida usuarios.');
-});
-
-module.exports = router;
