cb8bbb94e1e362fc5f27ac07c320bb73812b1e65
Kevin Paniagua <paniaguakevin2805@gmail.com>
2025-10-27 21:39:43 -0300

    Se remuevan archivos innecesarios

--- frontend/servidor_mvc/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Pantalla de bienvenida
-router.get('/', (req, res) => {
-    res.render('bienvenida');  // Renderiza la vista bienvenida.ejs
-});
-
-module.exports = router;
--- frontend/servidor_mvc/routes/routes_archivos.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send('Ruta de archivos en desarrollo. Disculpe las molestias.');
-});
-
-module.exports = router;
